8df3e8db060e7e474eb61e58b9f4663a9835a610
Adam <zsoriadam10@gmail.com>
2019-02-20 20:58:11 +0100

    vmi

--- wiki/wiki/Program.cs
+++ wiki/wiki/Program.cs
@@ -9,6 +9,22 @@ namespace wiki
     class Program
     {
         static void Main(string[] args) {
+            Console.SetWindowSize(80, 22);
+            string nev = "kkzs";
+            string parancs;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(nev);
+
+                for (int i = 0; i < 20; i++)
+                {
+                    Console.WriteLine();
+                }
+
+                parancs = Console.ReadLine();
+                //Console.CursorTop--;
+            } while (parancs != "q");
         }
     }
 }
